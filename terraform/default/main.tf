f1f71cf98d88f45abc1a21d7246fc3d1c8e232d1
quentin.guyon <quentin.guyon@flowbird.group>
2022-06-25 11:51:21 +0200

    Terraform de base

---
+++ terraform/default/main.tf
@@ -0,0 +1,26 @@
+resource "google_compute_instance" "default" {
+  name         = "flask-vm"
+  machine_type = "f1-micro"
+  zone         = "europe-west9-b"
+  tags         = ["ssh"]
+
+  metadata = {
+    enable-oslogin = "TRUE"
+  }
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  # Install Flask
+  metadata_startup_script = "sudo apt-get update; sudo apt-get install -yq build-essential python-pip rsync; pip install flask"
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      # Include this section to give the VM an external IP address
+    }
+  }
+}
